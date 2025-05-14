36ab9e15b1531cca4b2ec8cf046862612ca4936d
5uutari <46748210+5uutari@users.noreply.github.com>
2025-05-14 22:57:23 +0300

    Largest update to date
    Added a queue message when a song is added.
    Removed bloat.
    Updated requirements n shieeet...

--- soyniblerbot.py
+++ soyniblerbot.py
@@ -23,9 +23,10 @@ with open("config.json") as file:
 
 #making an olio for the song so you can match title and url in the same place easily
 class Song:
-    def __init__(self, songtitle, songurl):
+    def __init__(self, songtitle, songurl, duration):
         self.songtitle = songtitle
         self.songurl = songurl
+        self.duration = 0
 
 class CurrentlyPlaying:
     def __init__(self, song):
@@ -74,24 +75,37 @@ async def play(ctx, *, url: str):
     match = re.search(pattern, url)
 
     if match:
-        part = match.group(1)
+        videoid = match.group(1)
     else:
         await ctx.send("you did something illegal and YOU will be PUNISHED")
         return
 
-    saneUrl = f"https://www.youtube.com/watch?v={part}"
+    sanitized_url = f"https://www.youtube.com/watch?v={videoid}"
     ydl_opts = { "format": "bestaudio", "quiet": True, "no_warnings": True, "extract_flat": True, }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(saneUrl, download=False)
 
-    song = Song("", "")
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info(sanitized_url, download=False)
+
+        except yt_dlp.utils.DownloadError as e:
+            await ctx.send("Failed to fetch the video. Prob age restricted. Gross pervert...")
+            return
+
+    song = Song("", "", "")
 
     #this gives out an error for no reason, but it works and slaps the bees knees anyway...
     song.songtitle = info['title']
     song.songurl = info['url']
+    song.songduration = info['duration']
 
     songque.append(song)
+
+    if songque and ctx.voice_client.is_playing():
+        embed = discord.Embed(title="Added to queue", description=song.songtitle, color=discord.Color.red())
+        await ctx.send(embed=embed)
+
     print(f"added {song.songtitle} to queue")
+    print(f"song is {song.songduration} seconds long wow song long")
 
     #if bot is already playing epic memes on a channel then skip this, otherwise it starts blasting
     if not ctx.voice_client.is_playing():
@@ -115,7 +129,9 @@ async def playQue(ctx):
 #disintegrates the currently playing song and then continues on the queue
 @bot.command()
 async def skip(ctx):
-    if ctx.author.guild_permissions.administrator:
+    if ctx.author.guild_permissions.administrator and ctx.author.voice:
+        embed = discord.Embed(title="Skipping ", description=currentlyplaying.song, color=discord.Color.red())
+        await ctx.send(embed=embed)
         ctx.voice_client.stop()
     else:
         await ctx.send("ha skip on deez baalls :DDDDD")
@@ -131,32 +147,6 @@ async def queue(ctx):
     embed = discord.Embed(title="Currently playing", description=currentlyplaying.song, color=discord.Color.red())
     embed.set_footer(text=f"Next song is {songque[0].songtitle} \nQueue lenght {len(songque)}")
     await ctx.send(embed=embed)
-
-# if you belong to admin you get epic message, else
-@bot.command()
-async def servaa(ctx):
-    if ctx.author.guild_permissions.administrator:
-        await ctx.send(f"{ctx.author.display_name} bless your kind heart and have a nice day")
-    else:
-        await ctx.send(f"{ctx.author.display_name} you are a smelly sock and very ugly and gay")
-
-# if you belong to admin you get epic message, else
-@bot.command()
-async def poonis(ctx):
-    embed = discord.Embed()
-    embed.set_image(url="https://i.redd.it/izxiqwmhjvg31.jpg")
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def you(ctx):
-    if ctx.author.guild_permissions.administrator:
-        embed = discord.Embed(title=f"This is {ctx.author.display_name}", description="what a perfect being")
-        embed.set_image(url="https://static.wikia.nocookie.net/joke-battles/images/d/df/Gigachad.png/revision/latest?cb=20230812064835")
-        await ctx.send(embed=embed)
-    else:
-        embed = discord.Embed(title=f"This is literally {ctx.author.display_name}", description="see the pain in its eyes")
-        embed.set_image(url="https://pbs.twimg.com/media/FC229gYXsAQyWX5?format=jpg&name=large")
-        await ctx.send(embed=embed)
 
 @bot.command()
 async def naaduz(ctx):
